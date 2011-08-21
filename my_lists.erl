f44bef761a03bd5fa595499d90276a063c6a9a0f
Jeff Pollard <jeff.pollard@gmail.com>
2011-08-20 18:37:06 -0700

    Fix syntax error in my_lists and add max function.

--- my_lists.erl
+++ my_lists.erl
@@ -2,13 +2,21 @@
 -compile(export_all).
 
 min([H | T]) ->
-  min(H, T);
+  min(H, T).
 
 min(A, []) ->
   A;
-  
 min(A, [H | T]) when A < H ->
   min(A, T);
-  
 min(_A, [H | T]) ->
   min(H, T).
+
+max([H | T]) ->
+  max(H, T).
+
+max(A, []) ->
+  A;
+max(A, [H | T]) when A > H ->
+  max(A, T);
+max(_A, [H | T]) ->
+  max(H, T).
