ebceae6a5fd5da03b50175a2cd36651bb53ff096
Jeff Pollard <jeff.pollard@gmail.com>
2011-08-21 11:50:58 -0700

    Concurrency is fun!

---
+++ concurrency.erl
@@ -0,0 +1,27 @@
+-module (concurrency).
+-export ([start/0, listen/0]).
+
+ping(Listener, 0) ->
+  Listener ! finished,
+  io:format("Sending finished to the listener~n", []);
+ping(Listener, Number) ->
+  Listener ! {ping, self()},
+  receive
+    pong ->
+      io:format("Pinger received its pong~n", [])
+  end,
+  ping(Listener, Number - 1).
+
+listen() ->
+  receive
+    finished ->
+      io:format("Listener is finished. Goodbye!~n", []);
+    {ping, From} ->
+      io:format("Listener received ping!~n", []),
+      From ! pong,
+      listen()
+  end.
+
+start() ->
+  Listener = spawn(concurrency, listen, []),
+  ping(Listener, 5).
