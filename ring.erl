909f574f47599899079f79ecdaedc8095880f1de
Jeff Pollard <jeff.pollard@gmail.com>
2011-08-21 20:04:06 -0700

    Working ring passing messages.

--- ring.erl
+++ ring.erl
@@ -1,19 +1,25 @@
 -module(ring).
--export([start/2, build/1, listen_and_forward_to/1]).
+-export([start/3, build/1, listen_and_forward_to/2]).
 
-start(Size, Message) ->
-  LastPid = build(Size - 1),
-  LastPid ! Message,
-  listen_and_forward_to(LastPid).
+start(Size, Message, Count) ->
+  LastPid = build(Size),
+  LastPid ! {Message, Count},
+  listen_and_forward_to(LastPid, true).
 
 build(Size) ->
-  Builder = fun(_Num, Pid) -> spawn(ring, listen_and_forward_to, [Pid]) end,
-  lists:foldl(Builder, self(), lists:seq(1, Size)).
+  Builder = fun(_Num, Pid) -> spawn(ring, listen_and_forward_to, [Pid, false]) end,
+  lists:foldl(Builder, self(), lists:seq(1, Size-1)).
 
-listen_and_forward_to(Pid) ->
-  io:format("Process ~p forwarding to ~p~n", [self(), Pid]),
+listen_and_forward_to(Pid, Last) ->
   receive
-    Message ->
-      io:format("Process ~p received ~p, forwarding to ~p~n", [self(), Message, Pid]),
-      Pid ! Message
+    {Message, 1} ->
+      Pid ! {Message, 1},
+      io:format("Process ~p received ~p (1), forwarding to ~p~n", [self(), Message, Pid]);
+    {Message, Count} ->
+      io:format("Process ~p received ~p (~p), forwarding to ~p~n", [self(), Message, Count, Pid]),
+      case Last of
+        true -> Pid ! {Message, Count - 1};
+        false -> Pid ! {Message, Count}
+      end,
+      listen_and_forward_to(Pid, Last)
   end.
