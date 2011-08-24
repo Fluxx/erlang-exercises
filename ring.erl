299cfe366ad92bdb2668ce91c40b2fbb63e18a8e
Jeff Pollard <jeff.pollard@gmail.com>
2011-08-23 22:02:10 -0700

    build/1 shouldn't need to worry about doing Size-1

--- ring.erl
+++ ring.erl
@@ -2,13 +2,13 @@
 -export([start/3, build/1, listen_and_forward_to/2]).
 
 start(Size, Message, Count) ->
-  LastPid = build(Size),
+  LastPid = build(Size - 1),
   LastPid ! {Message, Count},
   listen_and_forward_to(LastPid, true).
 
 build(Size) ->
   Builder = fun(_Num, Pid) -> spawn(ring, listen_and_forward_to, [Pid, false]) end,
-  lists:foldl(Builder, self(), lists:seq(1, Size-1)).
+  lists:foldl(Builder, self(), lists:seq(1, Size)).
 
 listen_and_forward_to(Pid, Last) ->
   receive
