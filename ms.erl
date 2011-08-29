9baac5ebc420337a2178b17818ae7c010f364778
Jeff Pollard <jeff.pollard@gmail.com>
2011-08-28 20:52:51 -0700

    Clean up the code some and use a module function as a fun.

--- ms.erl
+++ ms.erl
@@ -1,31 +1,30 @@
 -module (ms).
 -export ([start/1, spawn_slaves/1, master/1, slave/1, to_slave/2]).
 
-start(N) -> register(master, spawn(ms, spawn_slaves, [N])).
+start(N) ->
+  register(master, spawn(ms, spawn_slaves, [N])).
 
-to_slave(Message, N) -> master ! {Message, N}.
+to_slave(Message, N) ->
+  master ! {Message, N}.
 
 spawn_slaves(N) ->
   process_flag(trap_exit, true),
-  Builder = fun(X) -> {X, spawn_link(ms, slave, [X])} end,
-  master(lists:map(Builder, lists:seq(1, N))).
+  master(lists:map(fun spawn_slave/1, lists:seq(1, N))).
+
+spawn_slave(N) ->
+  {N, spawn_link(ms, slave, [N])}.
 
 slave_pid(N, Slaves) ->
-  {_N, Pid} = lists:keyfind(N, 1, Slaves),
-  Pid.
+  erlang:element(2, lists:keyfind(N, 1, Slaves)).
 
-replace_pid(N, NewPid, Slaves) ->
+replace_pid({N, NewPid}, Slaves) ->
   lists:keyreplace(N, 1, Slaves, {N, NewPid}).
 
 master(Slaves) ->
   receive
     {'EXIT', _FromPid, {N, die} } ->
-      io:format("Process ~p died. Spawning a new one...~n", [N]),
-      ReplacementPid = spawn_link(ms, slave, [N]),
-      io:format("Replacement PID ~p spawned~n", [ReplacementPid]),
-      master(replace_pid(N, ReplacementPid, Slaves));
+      master(replace_pid(spawn_slave(N), Slaves));
     {Message, N} ->
-      io:format("Sending ~p to slave ~p~n", [Message, N]),
       slave_pid(N, Slaves) ! Message,
       master(Slaves)
   end.
